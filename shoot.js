f8bd8839634c3570cb00661e0e9cca0ee01b41b2
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2021-02-08 06:26:22 +0900

    0208進捗
    シュートページ追加

--- shoot.js
+++ shoot.js
@@ -3,7 +3,7 @@ var mySwiper = new Swiper ('.swiper-container', {
     loop: true, // 最後のスライドまで到達した場合、最初に戻らずに続けてスライド可能にするか。
     speed: 600, // スライドが切り替わるトランジション時間(ミリ秒)。
     slidesPerView: 4, // 何枚のスライドを表示するか
-    spaceBetween: 10, // スライド間の余白サイズ(ピクセル)
+    spaceBetween: 50, // スライド間の余白サイズ(ピクセル)
     direction: 'horizontal', // スライド方向。 'horizontal'(水平) か 'vertical'(垂直)。effectオプションが 'slide' 以外は無効。
     effect: 'slide', // "slide", "fade"(フェード), "cube"(キューブ回転), "coverflow"(カバーフロー) または "flip"(平面回転)
  
