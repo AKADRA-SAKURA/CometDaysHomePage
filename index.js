08e9ed4a824752cf099e9d4dd55def1f12b13809
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2021-02-11 16:17:22 +0900

    トップページあらかた完成

--- index.js
+++ index.js
@@ -2,10 +2,3 @@ $(function(){
     $("#header").load("header.html");
     $("#footer").load("footer.html");
 });
-
-$(window).on('load resize', function(){
-    // navbarの高さを取得する
-    var height = $('.navbar').height();
-    // bodyのpaddingにnavbarんぼ高さを設定する
-    $('body').css('padding-top',height); 
-});
