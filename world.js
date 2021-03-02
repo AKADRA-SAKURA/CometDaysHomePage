98ddab8ed1c278d973e1e9c786c66fee87821b28
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2021-03-02 22:40:42 +0900

    世界観ページアニメーション、main上余白調整

---
+++ world.js
@@ -0,0 +1,12 @@
+$(function () {
+    $(window).scroll(function () {
+        const wHeight = $(window).height();
+        const scrollAmount = $(window).scrollTop();
+        $('.scrollanime').each(function () {
+            const targetPosition = $(this).offset().top;
+            if(scrollAmount > targetPosition - wHeight + 60) {
+                $(this).addClass("fadeInDown");
+            }
+        });
+    });
+});
