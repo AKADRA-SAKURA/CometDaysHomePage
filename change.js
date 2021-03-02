242d6ec89b8dd7538fc31316830c4008bfba6fd3
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2021-03-02 16:25:17 +0900

    ページ遷移アニメーション追加

---
+++ change.js
@@ -0,0 +1,11 @@
+$(window).on('load',function(){
+    $("#splash-logo").delay(800).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
+  
+    //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+    $("#splash").delay(1100).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+    
+        $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+  
+    });
+    //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+});
